bbe33ba22f683592c40c2766cc060913c995eb54
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2024-09-15 19:42:46 +0200

    change that message get split by every 1950 chars

--- bot.py
+++ bot.py
@@ -10,28 +10,27 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="!",intents=intents)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
-#Load private keys
+
 
 
 
 @bot.event
 async  def on_ready():
-  env_path = Path('.') / '.env'  # Pfad zur .env-Datei (im aktuellen Verzeichnis)
+  env_path = Path('.') / '.env'
   load_dotenv(dotenv_path=env_path)
   google_api_key= os.getenv("GOOGLE_AI_PK")
   genai.configure(api_key=google_api_key)
-  # nur zum testen am anfang
   print("bot läuft")
 
 @bot.event
 async def on_message(message: discord.Message):
   if message.author == bot.user: return
   response = model.generate_content(message.content)
-  
-  print(response.text)
-  await message.channel.send(response.text)
+  splitted_answer = [response.text[i:i + 1950] for i in range(0, len(response.text), 1950)]
+  for element in (splitted_answer):
+    await message.channel.send(element)
 
-env_path = Path('.') / '.env'  # Pfad zur .env-Datei (im aktuellen Verzeichnis)
+env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 discord_key = os.getenv("DISCORD_TOKEN")
 bot.run(discord_key)
