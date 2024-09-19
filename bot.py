10cd0ec9ee0b20b97de03bf443d495cd2b58c11c
Tim Liebhaber <timliebhaber@gmail.com>
2024-09-19 22:02:39 +0200

    refactor: Improve bot message handling and image attachment support

--- bot.py
+++ bot.py
@@ -4,15 +4,13 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import google.generativeai as genai
+import shutil
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!",intents=intents)
 model = genai.GenerativeModel("gemini-1.5-flash")
-
-
-
-
 
 @bot.event
 async  def on_ready():
@@ -25,9 +23,22 @@ async  def on_ready():
 @bot.event
 async def on_message(message: discord.Message):
   if message.author == bot.user: return
-  if message.content.startswith("!") == False:
-    return
-  response = model.generate_content(message.content)
+  if message.content.startswith("!") == False: return
+  try:
+      url = message.attachments[0].url            # check for an image, call exception if none found
+  except IndexError:
+      print("Error: No attachments")
+  else:
+      r = requests.get(url, stream = True)        # download the image
+      imageName = '1.jpg'
+      with open(imageName, 'wb') as out_file:
+          print('Saving image: ' + imageName)
+          shutil.copyfileobj(r.raw, out_file)
+          image_file = genai.upload_file(path= './1.jpg')
+  
+  try: response = model.generate_content([image_file, message.content])
+  except UnboundLocalError: response = model.generate_content(message.content)
+  
   splitted_answer = [response.text[i:i + 1950] for i in range(0, len(response.text), 1950)]
   for element in (splitted_answer):
     await message.channel.send(element)
